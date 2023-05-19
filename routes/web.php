28a86de4e3c2497f114735a9d10a1e6d4a726193
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-19 16:54:44 +0200

    added route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +21,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', [DashboardController::class, 'home'])->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin')->group(function(){
+    
+    Route::resource('posts', PostController::class)->middleware(['auth', 'verified']);
+    Route::get('/admin', [DashboardController::class, 'home']);
+});
+
 
 require __DIR__.'/auth.php';
